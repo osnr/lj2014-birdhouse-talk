11342b512b4671371fe9fad170367414825756f0
Omar Rizwan <omar.rizwan@gmail.com>
2014-07-20 17:08:54 -0700

    up to the end of counter bot

--- Primitive.elm
+++ Primitive.elm
@@ -1,11 +1,11 @@
 module Primitive where
 
-preview : (Int, Int) -> Element
+preview : a -> Element
 preview pos = container 500 60 middle
           <| color lightGray <| container 500 50 middle
                <| asText pos
 
 previewStream : Signal Time -> Signal Element
-previewStream xs = flow up <~ foldp (::) [] (preview <~ xs)
+previewStream xs = flow down <~ foldp (::) [] (preview <~ xs)
 
 main = previewStream (every second)
