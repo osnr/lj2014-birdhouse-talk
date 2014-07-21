3266fefae201403eedbf41670b0f89a169fac187
Omar Rizwan <omar.rizwan@gmail.com>
2014-07-21 09:21:36 -0700

    add EveryMNLake and some combinators stuff

--- Lifting.elm
+++ Lifting.elm
@@ -2,16 +2,20 @@ module Lifting where
 
 import Mouse
 
-preview : a -> Element
-preview pos = container 250 60 middle
-          <| color lightGray <| container 250 50 middle
-               <| asText pos
+preview : Bool -> Element
+preview pos = container 340 60 middle
+          <| color (case pos of 
+                      True -> green
+                      False -> red)
+          <| container 250 50 middle
+          <| asText pos
 
 previewStream : Signal Bool -> Signal Element
 previewStream xs = flow down <~ foldp (::) [] (preview <~ xs)
 
 label : String -> Signal Element -> Signal Element
-label s se = above <~ (constant (width 290 <| plainText s)) ~ se
+label s se = above <~ (constant (size 340 40 <| centered <| monospace <| toText s))
+                    ~ se
 
 main = beside <~ (label "Mouse.isDown =" (previewStream Mouse.isDown))
                ~ (label "lift not Mouse.isDown =" (previewStream (lift not Mouse.isDown)))
